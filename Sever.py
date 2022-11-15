43fccc9cc369007adea2a27f1f6bfd54847c3297
dave5252 <ddiener98@gmail.com>
2022-11-15 15:15:23 +0100

    Started with server and Loading the Data - exercises

---
+++ Sever.py
@@ -0,0 +1,96 @@
+
+import time
+from typing import Optional
+import requests
+from collections import defaultdict #The defaultdict is a subdivision of the dict class. Its importance lies in the fact that it allows each new key to be given a default value based on the type of dictionary being created
+
+from main import msgP
+
+url = 'https://speakeasy.ifi.uzh.ch'
+class Server:
+
+    @classmethod
+    def login(cls, username, password, url='https://speakeasy.ifi.uzh.ch'):
+        return requests.post(url=url + "/api/login", json={"username": username, "password": password})
+
+    def __init__(self, username, password, url='https://speakeasy.ifi.uzh.ch'):
+        self.agent_details = self.login(username, password)
+        self._url = url
+        self.password = password
+        self.username = username
+        self.session_token = self.agent_details['sessionToken']
+        self.chat_state = defaultdict(lambda: {'messages': defaultdict(dict), 'initiated': False, 'my_alias': None})
+        self.data =
+
+    def listen(self):
+        listen_freq = 3  # seconds
+        while True:
+            # check for all chatrooms
+            current_rooms = self.check_rooms(session_token=self.session_token)['rooms']
+            for room in current_rooms:
+                # ignore finished conversations
+                if room['remainingTime'] > 0:
+                    room_id = room['uid']
+                    if not self.chat_state[room_id]['initiated']:
+                        # send a welcome message and get the alias of the agent in the chatroom
+                        self.post_message(room_id=room_id, session_token=self.session_token,
+                                          message='Whats up withit vanilla face, you can send me any message and check if it is echoed in {} seconds.'.format(
+                                              listen_freq))
+                        self.chat_state[room_id]['initiated'] = True
+                        self.chat_state[room_id]['my_alias'] = room['alias']
+
+                    # check for all messages
+                    all_messages = self.check_room_state(room_id=room_id, since=0, session_token=self.session_token)[
+                        'messages']
+
+                    # you can also use ["reactions"] to get the reactions of the messages: STAR, THUMBS_UP, THUMBS_DOWN
+
+                    for message in all_messages:
+                        if message['authorAlias'] != self.chat_state[room_id]['my_alias']:
+
+                            # check if the message is new
+                            if message['ordinal'] not in self.chat_state[room_id]['messages']:
+                                self.chat_state[room_id]['messages'][message['ordinal']] = message
+                                print(
+                                    '\t- Chatroom {} - new message #{}: \'{}\' - {}'.format(room_id, message['ordinal'],
+                                                                                            message['message'],
+                                                                                            self.get_time()))
+
+                                #
+                                msgp = msgP(message["message"])
+                                response = msgp.parseMsg()
+
+                                self.post_message(room_id=room_id, session_token=self.session_token,
+                                                  message='Whats poppin my G, Got your messag: \'{}\' at {}.'.format(message['message'],
+                                                                                                   self.get_time()))
+            time.sleep(listen_freq)
+    # check for available rooms -->https://speakeasy.ifi.uzh.ch/client-swagger#/Chat/getApiRooms
+    def login(self, username: str, password: str):
+        agent_details = requests.post(url=url + "/api/login", json={"username": username, "password": password}).json()
+        print('- User {} successfully logged in with session \'{}\'!'.format(agent_details['userDetails']['username'], agent_details['sessionToken']))
+        return agent_details
+
+    def check_rooms(self, session_token: str):
+        return requests.get(url=url + "/api/rooms", params={"session": session_token}).json()
+
+    def check_room_state(self, room_id: str, since: int, session_token: str):
+        return requests.get(url=url + "/api/room/{}/{}".format(room_id, since), params={"roomId": room_id, "since": since, "session": session_token}).json()
+
+    def post_message(self, room_id: str, session_token: str, message: str):
+        tmp_des = requests.post(url=url + "/api/room/{}".format(room_id),
+                                params={"roomId": room_id, "session": session_token}, data=message).json()
+        if tmp_des['description'] != 'Message received':
+            print('\t\t Error: failed to post message: {}'.format(message))
+
+    def get_time(self):
+        return time.strftime("%H:%M:%S, %d-%m-%Y", time.localtime())
+
+    def logout(self):
+        if requests.get(url=url + "/api/logout", params={"session": self.session_token}).json()['description'] == 'Logged out':
+            print('- Session \'{}\' successfully logged out!'.format(self.session_token))
+    #
+if __name__ == '__main__':
+    username = 'david.diener_bot'
+    password = ('czLFGXbrspCK0Q')
+    demobot = Server(username, password)
+    demobot.listen()
---
+++ loadData.py
@@ -0,0 +1,81 @@
+import logging
+
+import numpy as np
+import pandas as pd
+import re, json, csv, rdflib
+from rdflib.term import URIRef, Literal
+
+# %%
+# define some prefixes
+WD = rdflib.Namespace('http://www.wikidata.org/entity/')
+WDT = rdflib.Namespace('http://www.wikidata.org/prop/direct/')
+DDIS = rdflib.Namespace('http://ddis.ch/atai/')
+RDFS = rdflib.namespace.RDFS
+SCHEMA = rdflib.Namespace('http://schema.org/')
+
+# %%
+# instantiate graph
+graph = rdflib.Graph().parse(r'C:\Users\David\Desktop\Chatbot\Data\14_graph.nt', format='turtle')
+# load the embeddings
+entity_emb = np.load(r'C:\Users\David\Desktop\Chatbot\Data\entity_embeds.npy')
+relation_emb = np.load(r'C:\Users\David\Desktop\Chatbot\Data\relation_embeds.npy')
+logging.info('Loading embeddings')
+# load the images
+# load the dictionaries
+with open(r'C:\Users\David\Desktop\Chatbot\Data\entity_ids.del', 'r') as ifile:
+    ent2id = {rdflib.term.URIRef(ent): int(idx) for idx, ent in csv.reader(ifile, delimiter='\t')}
+    id2ent = {v: k for k, v in ent2id.items()}
+with open(r'C:\Users\David\Desktop\Chatbot\Data\relation_ids.del', 'r') as ifile:
+    rel2id = {rdflib.term.URIRef(rel): int(idx) for idx, rel in csv.reader(ifile, delimiter='\t')}
+    id2rel = {v: k for k, v in rel2id.items()}
+
+convEnt = {ent: str(lb) for ent, lb in graph.subject_objects(RDFS.label)}
+convLib = {lb: ent for ent, lb in convEnt.items()}
+entities = set(graph.subjects()) | {s for s in graph.objects() if isinstance(s, URIRef)}
+# all subjects and the objects which isinstance of URIRef are entities
+predicates = set(graph.predicates())
+literals = {s for s in graph.objects() if isinstance(s, Literal)}
+
+# %%
+# load imdb data
+top250 = set(open(r'C:\Users\David\Desktop\Chatbot\Data\imdb-top-250.t').read().split('\n'))
+top250 -= {''}
+
+print(pd.DataFrame([
+    ('Top-250 coverage', '{:n}'.format(
+        len(top250 & {str(o) for o in graph.objects(None, WDT.P345) if o.startswith('tt')}))),
+    ('Entities with IMDb ID', '{:n}'.format(
+        len({str(o) for o in graph.objects(None, WDT.P345) if o.startswith('tt')}))),
+    ('Plots linked to a movie', '{:n}'.format(
+        len({qid for qid, plot in csv.reader(open(r'C:\Users\David\Desktop\Chatbot\Data\plots.csv', encoding='utf-8')) if
+             URIRef(qid) in entities}))),
+    ('Comments linked to a movie', '{:n}'.format(
+        len([qid for qid, rating, sentiment, comment in csv.reader(open(r'C:\Users\David\Desktop\Chatbot\Data\user-comments.csv')) if
+             URIRef(qid) in entities]))),
+    ('Movies having at least one comment', '{:n}'.format(
+        len({qid for qid, rating, sentiment, comment in csv.reader(open(r'C:\Users\David\Desktop\Chatbot\Data\user-comments.csv')) if
+             URIRef(qid) in entities}))),
+]))
+# %%
+ent_lit_preds = {p for s, p, o in graph.triples((None, None, None)) if isinstance(o, Literal)}
+ent_lit_preds
+print(pd.DataFrame([
+    ('# entities', '{:n}'.format(
+        len(entities))),
+    ('DDIS.rating', '{:n}'.format(
+        len(set(graph.subjects(DDIS.rating, None))))),
+    ('DDIS.tag', '{:n}'.format(
+        len(set(graph.subjects(DDIS.tag, None))))),
+    ('SCHEMA.description', '{:n}'.format(
+        len({s for s in graph.subjects(SCHEMA.description, None) if s.startswith(WD)}))),
+    ('RDFS.label', '{:n}'.format(
+        len({s for s in graph.subjects(RDFS.label, None) if s.startswith(WD)}))),
+    ('WDT.P18 (wikicommons image)', '{:n}'.format(
+        len(set(graph.subjects(WDT.P18, None))))),
+    ('WDT.P2142 (box office)', '{:n}'.format(
+        len(set(graph.subjects(WDT.P2142, None))))),
+    ('WDT.P345 (IMDb ID)', '{:n}'.format(
+        len(set(graph.subjects(WDT.P345, None))))),
+    ('WDT.P577 (publication date)', '{:n}'.format(
+        len(set(graph.subjects(WDT.P577, None))))),
+]))
